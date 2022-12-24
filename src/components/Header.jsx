a63bb0f40e387cc008cfe16d0aacb40769404fbc
TJahongir <jakhongirt@gmail.com>
2022-12-24 10:21:43 +0500

    v.9 ready

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Header.css";
+import { ReactComponent as MenuIcon } from "../assets/icons/menu.svg";
 
 let headerContainer = {
 	display: "flex",
@@ -8,22 +9,43 @@ let headerContainer = {
 };
 
 const Header = () => {
+	const [isExpanded, setIsExpanded] = useState(false);
+	const toggleExpanded = () =>
+		setIsExpanded((prevIsExpanded) => !prevIsExpanded);
+
 	return (
-		<div className="headerContainer" style={headerContainer}>
-			<div className="logo">TOKNSWAP</div>
-			<div className="menu">
-				<div className="menu-links">
-					<a href="/">App</a>
-					<a href="/">Products</a>
-					<a href="/">Build</a>
-					<a href="/">Careers</a>
-					<a href="/">About</a>
+		<>
+			<div className="headerContainer" style={headerContainer}>
+				<div className="logo">TOKNSWAP</div>
+				<div className="menu">
+					<div className="menu-links">
+						<a href="/">App</a>
+						<a href="/">Products</a>
+						<a href="/">Build</a>
+						<a href="/">Careers</a>
+						<a href="/">About</a>
+					</div>
 				</div>
+				<div className="wallet-btn">
+					<button className="primary">Enter App</button>
+				</div>
+				<button className="hamburger" onClick={toggleExpanded}>
+					<MenuIcon />
+				</button>
 			</div>
-			<div className="wallet-btn">
-				<button className="primary">Enter App</button>
-			</div>
-		</div>
+			{isExpanded && (
+				<div className="menu-overlay">
+					<div className="menu-links">
+						<a href="/">App</a>
+						<a href="/">Products</a>
+						<a href="/">Build</a>
+						<a href="/">Careers</a>
+						<a href="/">About</a>
+						<a href="/">Enter App</a>
+					</div>
+				</div>
+			)}
+		</>
 	);
 };
 
